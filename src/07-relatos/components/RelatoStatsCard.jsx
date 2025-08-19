35054fc822f18eaa61fe7f066bd1473bf6e24874
VittorHannu <133688911+VittorHannu@users.noreply.github.com>
2025-08-19 08:05:55 -0300

    refactor: Adjust spacing in RelatoStatsCard
    Reduce space between label and percentage bar.
    
    Increase space between icon/count and label.

--- src/07-relatos/components/RelatoStatsCard.jsx
+++ src/07-relatos/components/RelatoStatsCard.jsx
@@ -9,7 +9,7 @@ const RelatoStatsCard = ({ label, count, icon: Icon, path, iconTextColor, iconBg
     <Link to={path} className="block h-full">
       <div className="bg-white rounded-xl shadow-sm p-4 h-full flex flex-col justify-between hover:shadow-md transition-shadow duration-300">
         
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center mb-2">
           <div className={`p-2 rounded-lg ${iconBgColor}`}>
             <Icon className={`w-6 h-6 ${iconTextColor}`} />
           </div>
@@ -20,7 +20,7 @@ const RelatoStatsCard = ({ label, count, icon: Icon, path, iconTextColor, iconBg
           <p className="text-base font-semibold text-gray-600">{label}</p>
           
           {percentage > 0 && (
-            <div className="flex items-center gap-2 mt-2">
+            <div className="flex items-center gap-2 mt-1">
               <div className="w-full bg-gray-200 rounded-full h-4 relative overflow-hidden">
                 <div
                   className={`h-4 rounded-full ${progressBarColor} flex items-center justify-end pr-1`}
